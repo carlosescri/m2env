7b3fdebe492249b6d54cba0a90e1221d70e0e6bb
Carlos Escribano <carlos@doofinder.com>
2019-09-23 12:59:09 +0200

    rf

--- templates/install_plugin.sh
+++ templates/install_plugin.sh
@@ -4,3 +4,4 @@ cd /app
 composer require $1
 php -d memory_limit=2G bin/magento setup:upgrade
 php -d memory_limit=2G bin/magento setup:di:compile
+php -d memory_limit=2G bin/magento setup:static-content:deploy
